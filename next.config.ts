6536fcdf18067bd93bd4663b94eac11c1483551d
Yash-devloper <sharmayash6761@gmail.com>
2026-05-25 14:26:00 +0530

    301 Redirect

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,24 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-images: {
+  images: {
     domains: ["res.cloudinary.com"],
   },
-  
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "diginfoexpert.com",
+          },
+        ],
+        destination: "https://diginfo.ai/:path*",
+        statusCode: 301,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
