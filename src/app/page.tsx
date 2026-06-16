54d4075089333bfb15fcc43afd6435a975f81a2a
Yash-devloper <sharmayash6761@gmail.com>
2026-06-16 13:41:14 +0530

    remove leadership from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,6 @@ export default function Home() {
       <Hero />
       <CapabilitiesSection />
       <AboutSection />
-      <LeadershipSection />
       <WhySection />   
       <AISearchSection />
       <ProcessSection />
